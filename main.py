5e7c478392d3965b8568cbe2cec4bd6ba422addb
akacarldm <53160889+akacarldm@users.noreply.github.com>
2019-08-10 12:25:41 -0400

    added women pics

--- main.py
+++ main.py
@@ -61,56 +61,54 @@ def get_outfit(gender, formal, time, season):
         url = 'https://i.pinimg.com/736x/b7/35/71/b7357108d4e5fe872c672695f4f263d0--fashion-for-men-fashion-styles.jpg'
     elif gender == "male" and formal == "fancy" and time == "day" and season == "winter":
         url = 'https://cdn.lookastic.com/looks/overcoat-blazer-long-sleeve-shirt-original-5859.jpg'
-
     elif gender == "female" and formal == "casual" and time == "night" and season == "summer":
-        url = ''
+        url = 'https://i.pinimg.com/originals/e2/36/b5/e236b5bcb07b180943f363f7f4866797.jpg'
     elif gender == "female" and formal == "casual" and time == "night" and season == "spring":
-        url = ''
+        url = 'https://i.pinimg.com/736x/a9/f1/45/a9f1454c8d78164c50fb14e4147e4994.jpg'
     elif gender == "female" and formal == "casual" and time == "night" and season == "fall":
-        url = ''
+        url = 'https://i.pinimg.com/originals/e3/0b/e1/e30be12d6e7f4800e8d4185106144af2.jpg'
     elif gender == "female" and formal == "casual" and time == "night" and season == "winter":
-        url = ''
+        url = 'https://i.pinimg.com/736x/6d/2a/eb/6d2aebf18e7c3d7af50d011eb7172580.jpg'
     elif gender == "female" and formal == "casual" and time == "day" and season == "summer":
-        url = ''
+        url = 'https://i.pinimg.com/originals/d7/de/b0/d7deb0e44c6535d3a5996dd8f5ec1f3f.jpg'
     elif gender == "female" and formal == "casual" and time == "day" and season == "spring":
-        url = ''
+        url = 'https://i.pinimg.com/originals/fa/f3/e0/faf3e09fc185762469cfdf7032f4cfb6.jpg'
     elif gender == "female" and formal == "casual" and time == "day" and season == "fall":
-        url = ''
+        url = 'https://i.pinimg.com/originals/b1/30/97/b1309763d29c3b20d903718b3e01122d.jpg'
     elif gender == "female" and formal == "casual" and time == "day" and season == "winter":
-        url = ''
+        url = 'https://i.pinimg.com/originals/aa/72/d8/aa72d82c73fbf44666fafc19c05a3add.jpg'
     elif gender == "female" and formal == "business-casual" and time == "night" and season == "summer":
-        url = ''
+        url = 'https://images.nyandcompany.com/is/image/NewYorkCompany/productlist2/Cold-Shoulder-Sheath-Dress_07722285_950.jpg'
     elif gender == "female" and formal == "business-casual" and time == "night" and season == "spring":
-        url = ''
+        url = 'https://mauricesprodatg.scene7.com/is/image/mauricesProdATG/112205_C2260?$large$'
     elif gender == "female" and formal == "business-casual" and time == "night" and season == "fall":
-        url = ''
+        url = 'https://i.pinimg.com/736x/f1/e1/43/f1e143acd67e4174b246cea7a921022f--fashion-makeup-photography-yellow-pants.jpg'
     elif gender == "female" and formal == "business-casual" and time == "night" and season == "winter":
-        url = ''
+        url = 'https://i.pinimg.com/originals/86/75/d5/8675d5766a3ac4af60aca03e10b1d25f.jpg'
     elif gender == "female" and formal == "business-casual" and time == "day" and season == "summer":
-        url = ''
+        url = 'https://media.missguided.com/s/missguided/DE929237_set/1/black-extreme-ruched-bandeau-bodycon-midi-dress'
     elif gender == "female" and formal == "business-casual" and time == "day" and season == "spring":
-        url = ''
+        url = 'https://i.pinimg.com/originals/15/0a/63/150a6309c38fd8402f269e230ec4e8cf.jpg'
     elif gender == "female" and formal == "business-casual" and time == "day" and season == "fall":
-        url = ''
+        url = 'https://i.pinimg.com/736x/af/79/b3/af79b32cb3607a6ca23723497410c8ec--stylish-spring-outfits-chic-work-outfit-spring.jpg'
     elif gender == "female" and formal == "business-casual" and time == "day" and season == "winter":
-        url = ''
+        url = 'https://i.pinimg.com/474x/ca/a8/20/caa8207fe4c2a59146558353928a9f09--business-casual-womens-fashion-winter-business-casual-clothes.jpg'
     elif gender == "female" and formal == "fancy" and time == "night" and season == "summer":
-        url = ''
+        url = 'https://i.pinimg.com/originals/ba/ec/c9/baecc9350e35af4dbdb1ae80c5bb5081.jpg'
     elif gender == "female" and formal == "fancy" and time == "night" and season == "spring":
-        url = ''
+        url = 'https://photo.venus.com/im/19017681.jpg?preset=pl-hero'
     elif gender == "female" and formal == "fancy" and time == "night" and season == "fall":
-        url = ''
+        url = 'https://i.pinimg.com/736x/07/6b/01/076b01b0dfc54388c2b64d26bbe9c044--luxury-dresses-luxury-outfits.jpg'
     elif gender == "female" and formal == "fancy" and time == "night" and season == "winter":
-        url = ''
+        url = 'https://i.pinimg.com/originals/82/14/7e/82147ec31ad1ee2c55853bea247050b9.jpg'
     elif gender == "female" and formal == "fancy" and time == "day" and season == "summer":
-        url = ''
+        url = 'https://i.pinimg.com/originals/29/a0/f9/29a0f9b0e003e42742e65df72ded5ae3.jpg'
     elif gender == "female" and formal == "fancy" and time == "day" and season == "spring":
-        url = ''
+        url = 'https://www.shopakira.com/media/catalog/product/cache/1/small_image/600x906.75/9df78eab33525d08d6e5fb8d27136e95/f/a/falling-in-love-off-the-shoulder-mini-dress_light-pink_1.jpg'
     elif gender == "female" and formal == "fancy" and time == "day" and season == "fall":
-        url = ''
+        url = 'https://i.pinimg.com/736x/70/e5/34/70e5346429ac7a2dfc64af1f10832def.jpg'
     elif gender == "female" and formal == "fancy" and time == "day" and season == "winter":
-        url = ''
-
+        url = 'https://i.pinimg.com/originals/f6/6f/03/f66f03710fd79638ba5280379f08f754.jpg'
     return url
 
 class CssiUser(ndb.Model):
